67977e767e1a2e1aca54caefc858ffc46f48c8f9
IIT-Bhupendra <bhupendra.singh.che19@iitbhu.ac.in>
2020-10-08 12:36:26 +0530

    files Commited

---
+++ partials/_signout.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+    echo "Please wait , We are signing you out.";
+    session_destroy();
+    session_unset();
+    header("location: /forum_1/main.php?lo=true");
+    exit();
+
+?>
