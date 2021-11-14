66fc89aa295a1d6ee385691cb516f49492557832
IIT-Bhupendra <bhupendra.singh.che19@iitbhu.ac.in>
2021-11-15 02:36:13 +0530

    database hosted over cloud clever

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "idiscuss";
+    $server = "bfizooxdv5y7ss6iba6j-mysql.services.clever-cloud.com";
+    $username = "uczfmuguxda7r5ym";
+    $password = "s1fTGxBIdfkhGJmFsiQR";
+    $database = "bfizooxdv5y7ss6iba6j";
 
     $conn = mysqli_connect($server, $username, $password, $database);
 ?>
