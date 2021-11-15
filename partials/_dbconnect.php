b572dee059565db4ceefeecac3dd284b13972495
IIT-Bhupendra <bhupendra.singh.che19@iitbhu.ac.in>
2021-11-15 21:57:45 +0530

    Trial: 02 again

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,17 +1,17 @@
 <?php
 
-    $server = "bfizooxdv5y7ss6iba6j-mysql.services.clever-cloud.com";
-    $username = "uczfmuguxda7r5ym";
-    $password = "s1fTGxBIdfkhGJmFsiQR";
-    $database = "bfizooxdv5y7ss6iba6j";
+    // $server = "bfizooxdv5y7ss6iba6j-mysql.services.clever-cloud.com";
+    // $username = "uczfmuguxda7r5ym";
+    // $password = "s1fTGxBIdfkhGJmFsiQR";
+    // $database = "bfizooxdv5y7ss6iba6j";
 
     // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     // $dotenv->safeLoad();    
 
-    $server = $_ENV['SERVER_NAME'];
-    $username = $_ENV['USER_NAME'];
-    $password = $_ENV['USER_IDENTIFICATION'];
-    $database = $_ENV['DB_NAME'];
+    $server = getenv('SERVER_NAME');
+    $username = getenv('USER_NAME');
+    $password = getenv('USER_IDENTIFICATION');
+    $database = getenv('DB_NAME');
 
     // echo vardum($server);
 
