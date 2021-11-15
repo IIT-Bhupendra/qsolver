b3a241f2c89ebd1dd7a67f7cd63b64e5e0f5eef9
IIT-Bhupendra <bhupendra.singh.che19@iitbhu.ac.in>
2021-11-15 16:20:41 +0530

    env vars configured

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,9 +1,13 @@
 <?php
 
-    $server = "bfizooxdv5y7ss6iba6j-mysql.services.clever-cloud.com";
-    $username = "uczfmuguxda7r5ym";
-    $password = "s1fTGxBIdfkhGJmFsiQR";
-    $database = "bfizooxdv5y7ss6iba6j";
+    // $server = "bfizooxdv5y7ss6iba6j-mysql.services.clever-cloud.com";
+    // $username = "uczfmuguxda7r5ym";
+    // $password = "s1fTGxBIdfkhGJmFsiQR";
+    // $database = "bfizooxdv5y7ss6iba6j";
+    $server = getenv('SERVER_NAME');
+    $username = getenv('USER_NAME');
+    $password = getenv('USER_IDENTIFICATION');
+    $database = getenv('DB_NAME');
 
     $conn = mysqli_connect($server, $username, $password, $database);
 ?>
