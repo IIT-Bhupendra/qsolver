c3752c630c9e5e5ecf092c4b448fa2b905dcb3fb
IIT-Bhupendra <bhupendra.singh.che19@iitbhu.ac.in>
2021-11-15 21:30:59 +0530

    trial: 01

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -4,17 +4,16 @@
     // $username = "uczfmuguxda7r5ym";
     // $password = "s1fTGxBIdfkhGJmFsiQR";
     // $database = "bfizooxdv5y7ss6iba6j";
-    require_once realpath(__DIR__ . "/vendor/autoload.php");
 
-    use Dotenv\Dotenv;
-
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->safeLoad();    
+    // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    // $dotenv->safeLoad();    
 
     $server = getenv('SERVER_NAME');
     $username = getenv('USER_NAME');
     $password = getenv('USER_IDENTIFICATION');
     $database = getenv('DB_NAME');
+
+    // echo vardum($server);
 
     $conn = mysqli_connect($server, $username, $password, $database);
 ?>
