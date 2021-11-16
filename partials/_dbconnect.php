2b2ca8c889ea9f86a607c840ed74aa0325a6aea5
Bhupendra Singh <72293702+IIT-Bhupendra@users.noreply.github.com>
2021-11-16 17:39:38 +0530

    comments removed

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,19 +1,7 @@
 <?php
-
     $server = "bfizooxdv5y7ss6iba6j-mysql.services.clever-cloud.com";
     $username = "uczfmuguxda7r5ym";
     $password = "s1fTGxBIdfkhGJmFsiQR";
     $database = "bfizooxdv5y7ss6iba6j";
-
-    // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    // $dotenv->safeLoad();    
-
-    // $server = getenv('SERVER_NAME');
-    // $username = getenv('USER_NAME');
-    // $password = getenv('USER_IDENTIFICATION');
-    // $database = getenv('DB_NAME');
-
-    // echo vardum($server);
-
     $conn = mysqli_connect($server, $username, $password, $database);
 ?>
