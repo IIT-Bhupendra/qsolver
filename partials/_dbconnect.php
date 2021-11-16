646126e7aa3073a2c5528a3c11f64d41974a6103
Bhupendra Singh <72293702+IIT-Bhupendra@users.noreply.github.com>
2021-11-16 18:50:43 +0530

    TRIAL 04

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,7 +1,16 @@
-<?php
+<!-- <?php
     $server =  getenv('SERVER_NAME');
     $username = getenv('USER_NAME');
     $password = getenv('USER_IDENTIFICATION');
+    $database = getenv('DB_NAME');
+    $conn = mysqli_connect($server, $username, $password, $database);
+?> -->
+
+<?php
+    $server = "bfizooxdv5y7ss6iba6j-mysql.services.clever-cloud.com";
+    $username = "uczfmuguxda7r5ym";
+    $password = "s1fTGxBIdfkhGJmFsiQR";
+//     $database = "bfizooxdv5y7ss6iba6j";
     $database = getenv('DB_NAME');
     $conn = mysqli_connect($server, $username, $password, $database);
 ?>
