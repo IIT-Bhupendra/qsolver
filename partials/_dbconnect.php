47adaca87f94052b17ab97330eacad8470b4c470
Bhupendra Singh <72293702+IIT-Bhupendra@users.noreply.github.com>
2021-11-16 18:29:14 +0530

    just trying to access config vars from github repo

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -2,6 +2,7 @@
     $server = "bfizooxdv5y7ss6iba6j-mysql.services.clever-cloud.com";
     $username = "uczfmuguxda7r5ym";
     $password = "s1fTGxBIdfkhGJmFsiQR";
-    $database = "bfizooxdv5y7ss6iba6j";
+//     $database = "bfizooxdv5y7ss6iba6j";
+    $database = getenv('DB_NAME');
     $conn = mysqli_connect($server, $username, $password, $database);
 ?>
