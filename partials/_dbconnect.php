f58e571eeb55f913f25a6d332230d8213e1a1e90
Bhupendra Singh <72293702+IIT-Bhupendra@users.noreply.github.com>
2021-11-16 19:14:29 +0530

    UPDATE : 01.01

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -2,9 +2,7 @@
     $server = "bfizooxdv5y7ss6iba6j-mysql.services.clever-cloud.com";
     $username = "uczfmuguxda7r5ym";
     $password = "s1fTGxBIdfkhGJmFsiQR";
-    $database = "bfizooxdv5y7ss6iba6j";
-    echo getenv('DB_NAME');
-    echo "<br>";
+    $database = getenv('DB_NAME');
     echo getenv('SERVER_CODE');
     echo "<br>";
     echo getenv('USER_IDENTIFICATION');
