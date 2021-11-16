5822276577b570b1a722f7edff8e5bf4ff3d0b00
Bhupendra Singh <72293702+IIT-Bhupendra@users.noreply.github.com>
2021-11-16 18:38:24 +0530

    Update _dbconnect.php

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,7 +1,7 @@
 <?php
     $server =  getenv('SERVER_NAME');
     $username = getenv('USER_NAME');
-    $password = getenv('USER_IDENTIFICATION);
+    $password = getenv('USER_IDENTIFICATION');
     $database = getenv('DB_NAME');
     $conn = mysqli_connect($server, $username, $password, $database);
 ?>
